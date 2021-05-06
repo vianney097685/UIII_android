3d336dd9ca1131f19cf1536670b5840cc96c6c88
FlutLab.io <>
2021-05-06 03:52:10 +0000

    Initial commit

---
+++ lib/datos_contact.dart
@@ -0,0 +1,28 @@
+class Contacto {
+  final String nombrecompleto;
+  final String email;
+  const Contacto({this.nombrecompleto, this.email});
+} // fin de la clase contacto
+
+const loscontactos = <Contacto>[
+  Contacto(nombrecompleto:
+   'Hatziry Arellano ', email:
+    'Hatziryarellano@gmail.com'),
+  Contacto(nombrecompleto:
+   'Gabriela Portillo', email:
+    'Gabrielaportillo7@gmail.com'),
+  Contacto(nombrecompleto:
+   'Alexis Gutierrez', email:
+    'Alexis18@gmail.com'),
+  Contacto(nombrecompleto:
+   'Yahir Sanchez', email:
+    'Yahirsanchez22@gmail.com'),
+  Contacto(nombrecompleto:
+   'Auroris Alvarez', email:
+    'Auroris233@gmail.com'),
+  Contacto(nombrecompleto:
+   'Alvaro Roman', email:
+    'Alvaruroman89@gmail.com'),
+
+  
+]; //final de <Contacto>[]
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'frm_contacto.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Ejemplo de flutter', 
+    debugShowCheckedModeBanner: false, 
+    theme: ThemeData(primarySwatch: Colors.yellow), 
+    home: PaginaContacto()
+    ) //fin de material
+      ); // fin de runApp
+} // fin de main
